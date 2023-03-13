bf1e7e6d39a56d51b6fcd5925ec78d7a71be6544
NagababuKollu <117262225+NagababuKollu@users.noreply.github.com>
2023-03-13 18:51:21 +0530

    Added method

--- app/src/main/java/com/thenorthface/tnf/carthilsdk/MainActivity.kt
+++ app/src/main/java/com/thenorthface/tnf/carthilsdk/MainActivity.kt
@@ -2,10 +2,12 @@ package com.thenorthface.tnf.carthilsdk
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 import com.thenorthface.tnf.carthilsdk.databinding.ActivityMainBinding
 import com.thenorthface.tnf.cartlib.DisplayQuoteActivity
+import com.thenorthface.tnf.cartlib.MyMath
 import com.thenorthface.tnf.cartlib.SampleFragment
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -23,6 +25,8 @@ public class MainActivity : AppCompatActivity() {
         binding.button.setOnClickListener {
             startActivity(Intent(this, DisplayQuoteActivity::class.java))
         }
+
+        Toast.makeText(this,""+MyMath.plus(5,5), Toast.LENGTH_LONG).show()
 
             if (savedInstanceState == null) {
             supportFragmentManager
